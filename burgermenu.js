71e634363ee39e3ccb0641bd2b38eb2ff6dfd726
Bastien Vienet <bastien.vienet@jobtrek.ch>
2022-06-23 16:53:46 +0200

    Including a nav bar

---
+++ burgermenu.js
@@ -0,0 +1,29 @@
+const navSlide = () => {
+    // Variables
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    // Event listener on the burger button
+    burger.addEventListener('click', () => {
+
+        // Toggle the nav bar
+        nav.classList.toggle('nav-active');
+
+        //Animate the links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ""
+
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`
+            }
+        });
+
+        // Burger Animation
+        burger.classList.toggle('toggle')
+    });
+}
+
+// Calling the function
+navSlide();
