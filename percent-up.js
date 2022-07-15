d7de0a7aee0b0972e331c959ac9faf07ec2e1aa7
Bastien Vienet <bastien.vienet@jobtrek.ch>
2022-07-15 14:26:15 +0200

    Renoming and finishing the under construction page

---
+++ percent-up.js
@@ -0,0 +1,11 @@
+let percent = 0;
+let percentText = document.getElementById('percent');
+
+function incrementPercent() {
+    if (!(percent >= 100)) {
+        percent += 1;
+        percentText.innerText = percent + "%";
+    }
+}
+
+setInterval(incrementPercent, 50);
